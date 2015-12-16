9d793a50d32e44be60d93d5e210ce0af99752c95
Maxim Klippa <m.klippa@2gis.ru>
2015-12-16 19:11:46 +0600

    launch dto validation

--- LaunchSample.Domain/Models/Dtos/LaunchDto.cs
+++ LaunchSample.Domain/Models/Dtos/LaunchDto.cs
@@ -1,15 +1,89 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
 using LaunchSample.Core.Enumerations;
 
 namespace LaunchSample.Domain.Models.Dtos
 {
-	public class LaunchDto
+	public class LaunchDto : IDataErrorInfo
 	{
+		#region State Properties
+
 		public int Id { get; set; }
 		public string City { get; set; }
 		public DateTime StartDateTime { get; set; }
 		public DateTime EndDateTime { get; set; }
 		public DateTime Month { get; set; }
-		public LaunchStatus Status { get; set; } 
+		public LaunchStatus Status { get; set; }
+
+		#endregion // State Properties
+
+		#region IDataErrorInfo Members
+
+		string IDataErrorInfo.this[string propertyName]
+		{
+			get { return GetValidationError(propertyName); }
+		}
+
+		string IDataErrorInfo.Error { get { return null; } }
+
+		#endregion IDataErrorInfo Members
+
+		#region Validation
+
+		static readonly string[] ValidatedProperties = 
+		{ 
+			"City", 
+			"StartDateTime", 
+			"EndDateTime"
+		};
+
+		private string GetValidationError(string propertyName)
+		{
+			if (Array.IndexOf(ValidatedProperties, propertyName) < 0)
+			{
+				return null;
+			}
+
+			string error = null;
+
+			switch (propertyName)
+			{
+				case "City":
+					error = ValidateCity();
+					break;
+
+				case "StartDateTime":
+					error = ValidateStartDateTime();
+					break;
+
+				case "EndDateTime":
+					error = ValidateEndDateTime();
+					break;
+
+				default:
+					Debug.Fail("Unexpected property being validated on Launch: " + propertyName);
+					break;
+			}
+
+			return error;
+		}
+
+		private string ValidateEndDateTime()
+		{
+			throw new NotImplementedException();
+		}
+
+		private string ValidateStartDateTime()
+		{
+			throw new NotImplementedException();
+		}
+
+		private string ValidateCity()
+		{
+			return string.IsNullOrWhiteSpace(City) ? "City is missing" : null;
+		}
+
+		#endregion // Validation
 	}
 }
