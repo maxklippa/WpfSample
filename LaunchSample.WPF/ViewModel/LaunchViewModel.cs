7523c8176260f0e1a6c61b68f8c2d133254256e6
Maxim Klippa <m.klippa@2gis.ru>
2015-12-16 18:20:38 +0600

    add LaunchView

---
+++ LaunchSample.WPF/ViewModel/LaunchViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using LaunchSample.BLL.Services;
+using LaunchSample.Domain.Models;
+
+namespace LaunchSample.WPF.ViewModel
+{
+	public class LaunchViewModel : ViewModelBase, IDataErrorInfo
+	{
+		private readonly Launch _launch;
+		private readonly LaunchService _launchService;
+
+		public string this[string columnName]
+		{
+			get
+			{
+				throw new System.NotImplementedException();
+				
+			}
+		}
+
+		public string Error { get; private set; }
+	}
+}
