88965b611e174a3c5dcb6b5a45539e7694f0362a
Maxim Klippa <m.klippa@2gis.ru>
2015-12-22 16:27:55 +0600

    context menu for success launch

---
+++ LaunchSample.WPF/Converters/SuccessLaunchConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.CodeDom;
+using System.Globalization;
+using System.Windows.Data;
+using LaunchSample.Core.Enumerations;
+
+namespace LaunchSample.WPF.Converters
+{
+	[ValueConversion(typeof(LaunchStatus), typeof(bool))]
+	public class SuccessLaunchConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var status = (LaunchStatus) value;
+
+			return status == LaunchStatus.Success;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return null; 
+		}
+	}
+}
