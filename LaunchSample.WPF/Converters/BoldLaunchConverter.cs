94142bf4f7b744239826b01fd167397277ad22f4
Maxim Klippa <m.klippa@2gis.ru>
2015-12-22 15:45:15 +0600

    add BoldLaunchConverter

---
+++ LaunchSample.WPF/Converters/BoldLaunchConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace LaunchSample.WPF.Converters
+{
+	[ValueConversion(typeof(DateTime), typeof(bool))]
+	public class BoldLaunchConverter : IValueConverter
+	{
+		private const int FRONTIER_DAY = 20;
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var date = System.Convert.ToDateTime(value);
+
+			return FRONTIER_DAY < date.Day;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return null;
+		}
+	}
+}
